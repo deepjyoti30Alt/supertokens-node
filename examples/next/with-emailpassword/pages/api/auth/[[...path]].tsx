3f7fdef40a0e8e85603001c1d33ff8a0340fecd9
Deepjyoti Barman <joey31real@gmail.com>
2024-09-02 21:58:02 +0530

    Add a change to debug res signature being different in edge

--- examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
+++ examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
@@ -6,20 +6,66 @@ import { backendConfig } from "../../../config/backendConfig";
 
 supertokens.init(backendConfig());
 
-export default async function superTokens(req: NextApiRequest, res: NextApiResponse) {
+class CustomResponseWrapper {
+    private res: NextApiResponse;
+    private headers: Headers;
+    private statusCode: number;
+    private body: string;
+
+    constructor(res: NextApiResponse) {
+        this.res = res;
+        this.headers = new Headers();
+        this.statusCode = 200;
+        this.body = "";
+    }
+
+    setHeader(name: string, value: string): void {
+        if (typeof this.res.setHeader === "function") {
+            this.res.setHeader(name, value);
+        } else {
+            this.headers.set(name, value);
+        }
+    }
+
+    status(code: number): this {
+        if (typeof this.res.status === "function") {
+            this.res.status(code);
+        } else {
+            this.statusCode = code;
+        }
+        return this;
+    }
+
+    send(body: string): Response | void {
+        if (typeof this.res.send === "function") {
+            return this.res.send(body);
+        } else {
+            this.body = body;
+            return new Response(this.body, {
+                status: this.statusCode,
+                headers: this.headers,
+            });
+        }
+    }
+
+    get writableEnded(): boolean {
+        return typeof this.res.writableEnded !== "undefined" ? this.res.writableEnded : false;
+    }
+}
+
+export default async function superTokens(req: NextApiRequest, rawRes: NextApiResponse): Promise<void | Response> {
+    const res = new CustomResponseWrapper(rawRes);
+
     await superTokensNextWrapper(
         async (next) => {
-            // This is needed for production deployments with Vercel
-            // It'll be overwritten by the middleware in some cases (the jwks.json endpoint)
-            console.log('res is: ', res);
-            // res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
-
+            res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
             await middleware()(req as any, res as any, next);
         },
         req,
-        res
+        res as any
     );
+
     if (!res.writableEnded) {
-        res.status(404).send("Not found");
+        return res.status(404).send("Not found");
     }
 }
