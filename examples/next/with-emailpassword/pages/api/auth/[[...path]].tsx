063f66f07fb9865eb3c2d373c767a213e589f001
Deepjyoti Barman <joey31real@gmail.com>
2024-09-03 14:57:16 +0530

    Add changes in pages to run it with app directory handler

--- examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
+++ examples/next/with-emailpassword/pages/api/auth/[[...path]].tsx
@@ -1,23 +1,35 @@
 import { superTokensNextWrapper } from "supertokens-node/nextjs";
-import { middleware } from "supertokens-node/framework/express";
+// import { middleware } from "supertokens-node/framework/express";
 import { NextApiRequest, NextApiResponse } from "next";
 import supertokens from "supertokens-node";
 import { backendConfig } from "../../../config/backendConfig";
+import { getAppDirRequestHandler } from "supertokens-node/nextjs";
+import { NextResponse } from "next/server";
 
 supertokens.init(backendConfig());
 
+const handleCall = getAppDirRequestHandler(NextResponse);
+
 export default async function superTokens(req: NextApiRequest, res: NextApiResponse) {
     await superTokensNextWrapper(
-        async (next) => {
+        async () => {
+            const response = await handleCall(req as any);
+            
             // This is needed for production deployments with Vercel
-            // It'll be overwritten by the middleware in some cases (the jwks.json endpoint)
-            res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            if (!response.headers.has("Cache-Control")) {
+                response.headers.set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            }
 
-            await middleware()(req as any, res as any, next);
+            // Set the headers and status code on the Next.js response
+            response.headers.forEach((value, key) => {
+                res.setHeader(key, value);
+            });
+            res.status(response.status).send(response.body);
         },
         req,
         res
     );
+
     if (!res.writableEnded) {
         res.status(404).send("Not found");
     }
