d824edb9afd84f41d1f43d44d8e26128bfe099ff
Deepjyoti Barman <joey31real@gmail.com>
2024-09-04 11:34:50 +0530

    Add some fixes to test pages based routing in edge

---
+++ examples/next/with-emailpassword/pages/api/auth/[...path].ts
@@ -0,0 +1,18 @@
+import { getAppDirRequestHandler } from "supertokens-node/nextjs";
+import supertokens from "supertokens-node";
+import { NextRequest, NextResponse } from "next/server";
+import { backendConfig } from "../../../config/backendConfig";
+
+supertokens.init(backendConfig());
+const handleCall = getAppDirRequestHandler(NextResponse);
+
+export default async function handler(req: Request, _: Response) {
+    const res = await handleCall(new NextRequest(req));
+
+    // Add Cache-Control header if it's not already set
+    if (!res.headers.has("Cache-Control")) {
+        res.headers.set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+    }
+
+    return res;
+}
